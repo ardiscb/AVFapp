b43d34cf8c8f88926c58b29c701a6e53dae79a09
Courtney Ardis <ardiscb@fullsail.edu>
2012-11-28 22:41:12 -0500

    Added feed.html for Twitter feed. Can't get the feed to display on the page. Results console log though.

--- iOS/AVFapp/www/js/index.js
+++ iOS/AVFapp/www/js/index.js
@@ -1,5 +1,21 @@
 /*Courtney Ardis
 AVF 1212
-Demo App 1 */
+Demo App 2 */
 
-//Javascript
+$(function(){
+	$.getJSON("http://search.twitter.com/search.json?q=marvel%20comics", function(data) {
+		$("#feed")
+		.html("<p>Twitter Feed Load Succsessful.</p>");
+		console.log(data);
+		for (i=0, j=data.results.length; i<j; i++){
+			$("#twitterFeed").append(
+				"<li>" +
+				"<img src='" + data.results[i].profile_image_url + "' />" +
+				"<a>" + data.results[i].from_user + "</a>" +
+				"<p>" + data.results[i].text + "</p>" +
+				"</li>"
+			);
+		}
+	});
+});
+console.log($("#twitterFeed"));
