8e49150d4a27916ed6f52a0dd0ceb107ff8db01c
Courtney Ardis <ardiscb@fullsail.edu>
2012-11-30 00:44:06 -0500

    Added second external data source, TechSavvy. It also pulls in the data, console logs it, but doesn't display in app. Kept tweeking video, still does show/play in app.

--- iOS/AVFapp/www/js/index.js
+++ iOS/AVFapp/www/js/index.js
@@ -2,19 +2,68 @@
 AVF 1212
 Demo App 2 */
 
-$(function(){
-	$.getJSON("http://search.twitter.com/search.json?q=marvel%20comics", function(data) {
-		$("#feed")
-		.html("<p>Twitter Feed Load Succsessful.</p>");
-		console.log(data);
+//AJAX call to get Twitter feeds about Marvel Comics
+$.ajax({
+    type: "GET",
+    url: "http://search.twitter.com/search.json?q=marvel%20comics&callback=?",
+    dataType: "jsonp",
+    success: function(data, response){
+    	$("#feed")		
+		.html("<p>Twitter Feed Load Successful.</p>");		
+		console.log(data);		
 		for (i=0, j=data.results.length; i<j; i++){
 			$("#twitterFeed").append(
-				"<li>" +
-				"<img src='" + data.results[i].profile_image_url + "' />" +
-				"<a>" + data.results[i].from_user + "</a>" +
-				"<p>" + data.results[i].text + "</p>" +
-				"</li>"
-			);
-		}
-	});
+				"<li>" +				
+				"<img src='" + data.results[i].profile_image_url + "' />" +				
+				"<a>" + data.results[i].from_user + "</a>" +				
+				"<p>" + data.results[i].text + "</p>" +				
+				"</li>"			
+			);		
+		}	
+    },
+    error: function(status, results){
+    	console.log(status, results);
+    }
 });
+
+//AJAX call to get job listings from techsavvy.io
+$.ajax({
+    type: "GET",
+    url: "http://api.techsavvy.io/jobs/javascript?limit=15&format=jsonp&callback=mySuperNeatCallback",
+    dataType: "jsonp",
+    success: function(data, response){
+    	$("#jobs")		
+		.html("<p>Job List Load Successful.</p>");		
+		console.log(data);		
+		for (i=0, j=data.length; i<j; i++){
+			$("#jobList").append(
+				"<li>" +				
+				"<img src='" + data[i].company_logo + "' />" +				
+				"<a>" + data[i].company + "</a>" +				
+				"<p>" + data[i].description + "</p>" +				
+				"</li>"			
+			);		
+		}	
+    },
+    error: function(status, results){
+    	console.log(status, results);
+    }
+});
+
+//First attempt - Console logs but doesn't display on the page.	
+// $(function(){
+// 	$.getJSON("http://search.twitter.com/search.json?q=marvel%20comics&callback=?", function(data) {
+// 		$("#feed")		
+// 		.html("<p>Twitter Feed Load Successful.</p>");		
+// 		console.log(data);		
+// 		for (i=0, j=data.results.length; i<j; i++){
+// 			$("#twitterFeed").append(
+// 				"<li>" +				
+// 				"<img src='" + data.results[i].profile_image_url + "' />" +				
+// 				"<a>" + data.results[i].from_user + "</a>" +				
+// 				"<p>" + data.results[i].text + "</p>" +				
+// 				"</li>"			
+// 			);		
+// 		}	
+// 	});
+// });
